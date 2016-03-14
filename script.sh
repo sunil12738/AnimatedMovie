0f40cdec0d53311cb2386e1304c0f554e2b3c4b4
sunil12738 <sunil12738@gmail.com>
2016-03-14 15:37:44 +0530

    script to run the files

---
+++ script.sh
@@ -0,0 +1,37 @@
+
+vlc scene1.mp3 &
+g++ scene1.cpp -lGL -lGLU -lglut -o run1
+./run1
+
+vlc scene2.mp3 &
+g++ scene2.cpp -lGL -lGLU -lglut -o run2
+./run2
+
+vlc scene3.mp3 &
+g++ scene3.cpp -lGL -lGLU -lglut -o run3
+./run3
+
+vlc scene4.mp3 &
+g++ scene4.cpp -lGL -lGLU -lglut -o run4
+./run4
+
+vlc scene5.mp3 &
+g++ scene5.cpp -lGL -lGLU -lglut -o run5
+./run5
+
+vlc scene67.mp3 &
+g++ scene6.cpp -lGL -lGLU -lglut -o run6
+./run6
+g++ scene7.cpp -lGL -lGLU -lglut -o run7
+./run7
+g++ scene8.cpp -lGL -lGLU -lglut -o run8
+./run8
+
+rm run1
+rm run2
+rm run3
+rm run4
+rm run5
+rm run6
+rm run7
+rm run8
